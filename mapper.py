58cdde7e92120640e317a6eab101782aea173431
Mahdi Malverdi <Mahdi.Malverdi@gmail.com>
2021-03-29 17:08:12 +0430

    add files

---
+++ mapper.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+import sys
+
+def read_input(file):
+ for line in file:
+  yield line.split()
+
+def main(separator='\t'):
+ data = read_input(sys.stdin)
+  for word in words:
+  print('%s%s%d' % (word, separator, 1))
+
+if __name__ == "__main__":
+ main()
+
