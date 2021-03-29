1fa4ade2568f37a382340558f2e361726159e964
Mahdi Malverdi <Mahdi.Malverdi@gmail.com>
2021-03-29 18:16:11 +0430

    fix bugs

--- mapper.py
+++ mapper.py
@@ -2,14 +2,17 @@
 import sys
 
 def read_input(file):
- for line in file:
-  yield line.split()
+    for line in file:
+        yield line.split(' ')
 
 def main(separator='\t'):
- data = read_input(sys.stdin)
-  for word in words:
-   print('%s%s%d' % (word, separator, 1))
+    lines = []
+    data = read_input(sys.stdin)
+    for line in data:
+        for word in line:
+            lines.append('%s%s%d' % (word, separator, 1))
+    print('\n'.join(lines))
 
 if __name__ == "__main__":
- main()
+    main()
 
--- reducer.py
+++ reducer.py
@@ -5,18 +5,21 @@ from itertools import groupby
 from operator import itemgetter
 import sys
 
+
 def read_mapper_output(file, separator='\t'):
- for line in file:
-  yield line.rstrip().split(separator, 1)
+    for line in file:
+        yield line.rstrip().split(separator, 1)
 
 def main(separator='\t'):
- data = read_mapper_output(sys.stdin, separator=separator)
- for current_word, group in groupby(data, itemgetter(0)):
-  try:
-   total_count = sum(int(count) for current_word, count in group)
-   print( "%s%s%d" % (current_word, separator, total_count))
-  except ValueError:
-   pass
+    lines = []
+    data = read_mapper_output(sys.stdin, separator=separator)
+    for current_word, group in groupby(data, itemgetter(0)):
+        try:
+            total_count = sum(int(count) for current_word, count in group)
+            lines.append("%s%s%d" % (current_word, separator, total_count))
+        except ValueError:
+            pass
+    print('\n'.join(lines))
 
 if __name__ == "__main__":
     main()
